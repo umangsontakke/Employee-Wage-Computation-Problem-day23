98c34449c126ac3468f3069673d78007ae8c4eac
umangsontakke <umangsontakke70@gmail.com>
2020-08-29 19:47:33 +0530

    PartTimeEmpWage file is added to uc3 branch

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,16 +1,19 @@
-kpublic class DailyEmpWage
+public class ParttimeEmpWage
 {
 	public static void main(String[] arg)
 	{
                   //Constants
-		int fullTimeEmp=1;
+		int IS_PART_TIME=1;
+		int IS_FULL_TIME=2;
 		int EMP_RATE_PER_HOUR=20;
 			//Variables
 		int empHrs=0;
 		int empWage=0;
 			//Computation
 		int empCheck=(Math.random()*10)%2;
-		if(empCheck==IS_FULL_TIME)
+		if(empCheck==IS_PART_TIME)
+		empHrs=4;
+		else if(empCheck==IS_FULL_TIME)
 		empHrs=8;
 		else
 		empHrs=0;
