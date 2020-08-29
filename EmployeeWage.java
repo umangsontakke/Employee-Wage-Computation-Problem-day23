6a57467cdd9e448755b64539a5122fd35e6104d2
umangsontakke <umangsontakke70@gmail.com>
2020-08-29 19:50:00 +0530

    EmpWageSwitchCase file is added to uc4 branch

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,22 +1,26 @@
-public class ParttimeEmpWage
+public class EmpWageSwitchCase
 {
+
+public static final int IS_PART_TIME=1;
+public static final int IS_FULL_TIME=2;
+public static final int EMP_RATE_PER_HOUR=20;
 	public static void main(String[] arg)
 	{
-                  //Constants
-		int IS_PART_TIME=1;
-		int IS_FULL_TIME=2;
-		int EMP_RATE_PER_HOUR=20;
-			//Variables
+                  //variables
 		int empHrs=0;
 		int empWage=0;
-			//Computation
+	           //computation
 		int empCheck=(Math.random()*10)%2;
-		if(empCheck==IS_PART_TIME)
-		empHrs=4;
-		else if(empCheck==IS_FULL_TIME)
-		empHrs=8;
-		else
-		empHrs=0;
+		switch(empCheck){
+		case IS_PART_TIME:
+			empHrs=4;
+			break;
+		case IS_FULL_TIME:
+			empHrs=8;
+			break;
+		default:
+			empHrs=0;
+	}
 		empWage=empHrs*EMP_RATE_PER_HOUR;
 		System.out.println("Emp Wage="+empWage);
 	    	
