618b85301a50745e101d118a43981b97ca77bf73
umangsontakke <umangsontakke70@gmail.com>
2020-08-29 19:56:29 +0530

    EmpWorkingHrs file is added to uc6 branch

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,22 +1,24 @@
-public class EmpWageMonth
+public class TotalWorkinghrsMonth
 {
-	//instance variable
+
 public static final int IS_PART_TIME=1;
 public static final int IS_FULL_TIME=2;
 public static final int EMP_RATE_PER_HOUR=20;
 public static final int NUM_OF_WORKING_DAYS=2;
+public static final int MAX_HRS_IN_MONTH=10;
 	public static void main(String[] arg)
 	{
                   //variables
 		int empHrs=0;
-		int empWage=0;
-		int totalEmpWage=0;
+		int totalempHrs=0;
+		int totalWorkingDays=0;
 	           //computation
-		for(int day=0;day<NUM_OF_WORKING_DAYS;day++)
-	   {
-		int empCheck=(int)(Math.random()*10)%3;
-		switch(empCheck)
-	      {
+		while(totalEmpHrs<=MAX_HRS_IN_MONTH &&
+		totalworkingdays<NUM_OF_WORKING_DAYS)
+		{
+		totalWorkingDays++;
+                  int empCheck=(int)(Math.random()*10)%3;
+		switch(empCheck){
 		case IS_PART_TIME:
 			empHrs=4;
 			break;
@@ -25,13 +27,12 @@ public static final int NUM_OF_WORKING_DAYS=2;
 			break;
 		default:
 			empHrs=0;
-	       }
-		empWage=empHrs*EMP_RATE_PER_HOUR;
+	        }
+		
 		totalEmpWage+=empWage;
-		System.out.println("Emp Wage="+empWage);
-	    }
+		System.out.println("Day#:"+TotalWorkingDays+"Emp Hr:"+empHrs);
+        }
+		int totalEmpWage=totalEmpHrs*EMP_RATE_PER_HOUR;
 		System.out.println("Total Emp Wage="+totalEmpWage);	
-
-	}
 
 }
