80b81cfdcf03a7771b2866096fc5ba78f49113a2
umangsontakke <umangsontakke70@gmail.com>
2020-08-29 20:11:22 +0530

    EmpWageForEachCompany file is added to uc9 branch

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,72 +1,79 @@
-public class EmpWageInstance {
-	
-	//instance variables
+// instance variables
+		private String companyName;
+		private int empRate;
+		private int numOfWorkingDays;
+		private int maxHrsInMonth;
+		private int totalEmpWage;
 
-	private int empRate;
-	private int numOfWorkingDays;
-	private int maxHrsInMonth;
-
-	//instance method
-	public EmpWageInstance(final int empRate, final int numOfWorkingDays, final int maxHrsInMonth) {
-		this.empRate = empRate;
-		this.numOfWorkingDays = numOfWorkingDays;
-		this.maxHrsInMonth = maxHrsInMonth;
-	}
-
-	public static void main(String[] args) {
-		final EmpWageInstance ewi = new EmpWageInstance(20, 20, 100);
-		ewi.caldailyEmpWage();
-	}
-
-	/**
-	 * calculate employee daily wages based on type of employee
-	 */
-
-	public void caldailyEmpWage() {
-		
-		//local variables
-		int totalWage = 0;
-		int totalEmpHrs = 0;
-		int totalWorkingDays = 0;
-		while (totalEmpHrs < maxHrsInMonth && totalWorkingDays<numOfWorkingDays) {
-			totalWorkingDays++;
-
-			final int empHrs = getEmpHrs();
-			final int empWage = empHrs * empRate;
-			totalEmpHrs = empHrs;
-			totalWage = empWage;
-			System.out.println("EMP day=" + totalWorkingDays + "wages=" + empWage);
+		public EmpWageInstance(final String companyName, final int empRate, final int numOfWorkingDays, final int maxHrsInMonth) {
+			
+			this.companyName=companyName;
+			this.empRate = empRate;
+			this.numOfWorkingDays = numOfWorkingDays;
+			this.maxHrsInMonth = maxHrsInMonth;
 		}
-		System.out.println("Total emp Wage=" + totalWage);
-	}
 
-	/* @return employee hrs */
-	public int getEmpHrs() {
-		
-		//local variables
-		
-		final int isFullTime = 1;
-		final int isPartTime = 2;
-		int empHrs = 0;
+		public static void main(String[] args) {
+			final EmpWageUc9 ewMicrosoft = new EmpWageUc9("Microsoft", 20, 20, 100);
+			ewMicrosoft.computeEmpWage();
+			System.out.println("Total Emp Wage of Microsift company=" + ewMicrosoft);
 
-                //get random value
-		final double randomValue = Math.floor(Math.random() * 10) % 3;
+			final EmpWageUc9 ewApple = new EmpWageInstance("Apple", 20, 18, 100);
+			ewApple.computeEmpWage();
+			System.out.println("Total Emp Wage of Apple company=" + ewApple);
 
-		switch ((int) randomValue) {
+			final EmpWageUc9 ewHtc = new EmpWageInstance("Htc", 20, 20, 120);
+			ewHtc.computeEmpWage();
+			System.out.println("Total Emp Wage of Htc Company=" + ewHtc);
+		}
 
-		case isFullTime:
-			empHrs = 8;
-			System.out.println("Emp is Present for full time");
-			break;
-		case isPartTime:
-			empHrs = 4;
-			break;
-		default:
-			System.out.println("Emp is absent");
-			break;
+		/**
+		 * calculate employee daily wages based on type of employee
+		 */
+
+		public int computeEmpWage() {
+
+			// local Variables
+			int totalWage = 0;
+			int totalEmpHrs = 0;
+			int totalWorkingDays = 0;
+			while (totalEmpHrs < maxHrsInMonth && totalWorkingDays < numOfWorkingDays) {
+				totalWorkingDays++;
+
+				final int empHrs = getEmpHrs();
+				final int empWage = empHrs * empRate;
+				totalEmpHrs = empHrs;
+				totalWage = empWage;
+				System.out.println("EMP day=" + totalWorkingDays + "wages=" + empWage);
+			}
+			totalEmpWage=totalWage;
+		}
+
+		/* @return employee hrs */
+		public int getEmpHrs() {
+
+			// local variables
+			final int isFullTime = 1;
+			final int isPartTime = 2;
+			int empHrs = 0;
+
+	//get random value
+			final double randomValue = Math.floor(Math.random() * 10) % 3;
+
+			switch ((int) randomValue) {
+
+			case isFullTime:
+				empHrs = 8;
+				System.out.println("Emp is Present for full time");
+				break;
+			case isPartTime:
+				empHrs = 4;
+				break;
+			default:
+				System.out.println("Emp is absent");
+				break;
+
+			}
+			return empHrs;
 
 		}
-		return empHrs;
-
-	}
-}
