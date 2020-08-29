594e751696241d6abe50195b9538e88b5b62ee54
umangsontakke <umangsontakke70@gmail.com>
2020-08-29 19:53:54 +0530

    EmpWageMonth file is added to uc5 branch

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,17 +1,22 @@
-public class EmpWageSwitchCase
+public class EmpWageMonth
 {
-
+	//instance variable
 public static final int IS_PART_TIME=1;
 public static final int IS_FULL_TIME=2;
 public static final int EMP_RATE_PER_HOUR=20;
+public static final int NUM_OF_WORKING_DAYS=2;
 	public static void main(String[] arg)
 	{
                   //variables
 		int empHrs=0;
 		int empWage=0;
+		int totalEmpWage=0;
 	           //computation
-		int empCheck=(Math.random()*10)%2;
-		switch(empCheck){
+		for(int day=0;day<NUM_OF_WORKING_DAYS;day++)
+	   {
+		int empCheck=(int)(Math.random()*10)%3;
+		switch(empCheck)
+	      {
 		case IS_PART_TIME:
 			empHrs=4;
 			break;
@@ -20,10 +25,12 @@ public static final int EMP_RATE_PER_HOUR=20;
 			break;
 		default:
 			empHrs=0;
-	}
+	       }
 		empWage=empHrs*EMP_RATE_PER_HOUR;
+		totalEmpWage+=empWage;
 		System.out.println("Emp Wage="+empWage);
-	    	
+	    }
+		System.out.println("Total Emp Wage="+totalEmpWage);	
 
 	}
 
