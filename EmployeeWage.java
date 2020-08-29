25ee7b4f8b122a8a2f8e7b47cefd924b877f56fd
umangsontakke <umangsontakke70@gmail.com>
2020-08-29 19:29:39 +0530

    EmployeeWage file is added to uc1 branch

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,2 +1,24 @@
-dadasd
+public class CheckEmpPresentAbsent
+{
+	public static void main(String[] arg)
+	{
+                  //Constants
+		int fullTimeEmp=1;
+	    	int randomCheck=(int)((Math.random()*10)%2);
+		System.out.println(randomCheck);	
+		if(fullTimeEmp==randomCheck)
+	{
+		System.out.println("empoyee is present");
 
+	}
+	else
+	{
+		System.out.println("employee is absent");	
+
+	}
+
+}
+
+
+}
+© 2020 GitHub, Inc.
